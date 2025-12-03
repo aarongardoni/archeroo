69551ff118832aa626d6bba3688f5ba278d33def
Aaron Gardoni <aarongardoni@gmail.com>
2025-12-03 12:27:07 +1100

    feat: arch rice v0.0.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Arch Linux
+
+This is my custom Arch Linux setup.
+
+It's been inspired by trying out Omarchy.
+
+TODO - Finish this README.
+
+![Screenshot][./image.png]
+
---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo
+echo "--- Creating sym links"
+echo
+pushd bin;
+  for item in *; do
+    # Check if the item is a regular file
+    if [ -f "$item" ]; then
+      chmod +x $item
+      if [ -f "/usr/bin/$item" ]; then
+        echo "symbolic link exists for "$(pwd)/$item" -> /usr/bin/$item"
+      else
+        sudo ln --symbolic --verbose "$(pwd)/$item" /usr/bin/
+      fi
+    fi
+  done
+popd
+
+echo
+echo "--- Updating desktop applications"
+echo
+pushd /usr/share/applications;
+  # TODO: This should come from packages or something
+  apps=("zen" "slack" "ghostty" "bolt-launcher" "firefox" "nvim" "spotify" "discord")
+
+  for file in *; do
+    # Check if the item is a regular file
+    if [ -f "$file" ]; then
+      found=false
+
+      for app in "${apps[@]}"; do
+        if [[ "$file" == *"$app"* ]]; then
+          found=true
+          break
+        fi
+      done
+
+      # It's not in apps we want to hide it
+      if [[ "$found" == "false" ]]; then
+        if grep -q "Hidden=true" $file; then
+          echo "hidden already $file"
+        else
+          echo "hidden $file"
+          sudo echo "Hidden=true" | sudo tee -a $file
+        fi
+      else
+        echo "not hidden $file"
+      fi
+    fi
+
+  done
+popd
+
+echo
+echo "--- Copying files"
+echo
+
+mkdir -p ~/wallpapers/
+mkdir -p ~/screenshots/
+mkdir -p ~/videos/
+
+cp -rv config/* ~/.config/
+cp -rv wallpapers/* ~/wallpapers/
+
