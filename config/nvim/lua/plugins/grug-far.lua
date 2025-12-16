caaf74c9ebce55f2ce20a26501cc6b78229e2bfe
Aaron Gardoni <aarongardoni@gmail.com>
2025-12-16 12:04:03 +1100

    feat: swap out spectre for grug-far

---
+++ config/nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'MagicDuck/grug-far.nvim',
+    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+    -- additional lazy config to defer loading is not really needed...
+    keys = {
+      {
+        '<leader>sr',
+        function()
+          local grug = require 'grug-far'
+          local ext = vim.bo.buftype == '' and vim.fn.expand '%:e'
+          grug.open {
+            transient = true,
+            prefills = {
+              filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
+            },
+          }
+        end,
+        mode = { 'n', 'x' },
+        desc = 'Search and Replace',
+      },
+    },
+  },
+}
