0384c21321105c91bf55b1d50257948c447617a3
Aaron Gardoni <aarongardoni@gmail.com>
2026-05-14 18:12:29 +1000

    chore: fix tree sitter for v12 + endec for b64

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -204,6 +204,8 @@ return {
       local servers = {
         clangd = {},
         gopls = {},
+        zls = {},
+        ols = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
